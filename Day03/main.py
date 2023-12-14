c20569f8f43b9dc60d7f7a5a71652670c919d642
jsmaskeen <jsmaskeen@gmail.com>
2023-12-14 17:53:07 +0530

    day 3

--- Day03/main.py
+++ Day03/main.py
@@ -7,14 +7,31 @@ class Day3:
     def part1(self,inp=None):
         if not inp:inp=self.input
         v = 0
+        for i in inp:
+            c1,c2 = set(i[:len(i)//2]),set(i[len(i)//2:])
+            a = list(c1.intersection(c2))[0]
+            if a.islower():
+                v+= 1 + ord(a) - ord('a')
+            else:
+                v+= 1 + ord(a) - ord('A')+26
+
+
         print(f'Part1: {v}')
 
         
 
     def part2(self,inp=None):
         if not inp:inp=self.input
-        u = 0
-        print(f'Part2: {u}')
+        v = 0
+        for i in range(len(inp)//3):
+            ls = inp[3*i:3*(i+1)]
+            a = list(set(ls[0]).intersection(set(ls[1])).intersection(set(ls[2])))[0]
+            if a.islower():
+                v+= 1 + ord(a) - ord('a')
+            else:
+                v+= 1 + ord(a) - ord('A')+26
+
+        print(f'Part2: {v}')
 
 
 
@@ -34,5 +51,5 @@ class Day3:
             print()
             self.part2()
 
-solver = Day3('test.txt','input.txt',True)
+solver = Day3('test.txt','input.txt',)
 solver.solve()
