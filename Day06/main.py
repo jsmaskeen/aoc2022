85adf28531dc8d38ea0f4077fa183e04172fe31a
jsmaskeen <jsmaskeen@gmail.com>
2023-12-14 18:50:12 +0530

    day 6

--- Day06/main.py
+++ Day06/main.py
@@ -1,12 +1,20 @@
 class Day6:
     def __init__(self,test,inp,run_test=False) -> None:
-        self.test = open(test).read().split('\n')
-        self.input = open(inp).read().split('\n')
+        self.test = open(test).read()
+        self.input = open(inp).read()
         self.run_test = run_test
+
     
     def part1(self,inp=None):
         if not inp:inp=self.input
         v = 0
+
+        for i in range(3,len(inp)):
+            if len(set(inp[i-4:i])) == 4:
+                v = i
+                break
+            
+
         print(f'Part1: {v}')
 
         
@@ -14,6 +22,10 @@ class Day6:
     def part2(self,inp=None):
         if not inp:inp=self.input
         u = 0
+        for i in range(13,len(inp)):
+            if len(set(inp[i-14:i])) == 14:
+                u = i
+                break
         print(f'Part2: {u}')
 
 
@@ -34,5 +46,5 @@ class Day6:
             print()
             self.part2()
 
-solver = Day6('test.txt','input.txt',True)
+solver = Day6('test.txt','input.txt',0)
 solver.solve()
