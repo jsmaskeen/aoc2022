e189274b6ef1f68585a39869178fa6fb93e193eb
jsmaskeen <jsmaskeen@gmail.com>
2023-12-14 18:27:43 +0530

    day 5

--- Day05/main.py
+++ Day05/main.py
@@ -1,20 +1,71 @@
 class Day5:
     def __init__(self,test,inp,run_test=False) -> None:
-        self.test = open(test).read().split('\n')
-        self.input = open(inp).read().split('\n')
+        self.test = open(test).read().split('\n\n')
+        self.input = open(inp).read().split('\n\n')
         self.run_test = run_test
     
     def part1(self,inp=None):
         if not inp:inp=self.input
-        v = 0
+        v = ''
+        crats = inp[0].split('\n')
+        maax = int(crats[-1].replace(' ','')[-1])
+        gg = []
+        for j in crats[:-1]:
+            tmp = []
+            for i in range(maax):
+                tmp.append(j[4*i:4*(i+1)].strip('[] '))
+            gg.append(tmp)
+        rows = [[j for j in i if j!=''][::-1] for i in list(zip(*gg))]
+        instruct = inp[1].split('\n')
+
+        for i in instruct:
+            qt,fro,to = list(map(int,i.split(' ')[1::2]))
+            j = 0
+            while j!=qt:
+                rows[to-1].append(rows[fro-1].pop())
+                j+=1
+        
+        
+        for i in rows:
+            v+=i[-1]
+
         print(f'Part1: {v}')
 
         
 
     def part2(self,inp=None):
         if not inp:inp=self.input
-        u = 0
-        print(f'Part2: {u}')
+        v = ''
+        crats = inp[0].split('\n')
+        maax = int(crats[-1].replace(' ','')[-1])
+        gg = []
+        for j in crats[:-1]:
+            tmp = []
+            for i in range(maax):
+                tmp.append(j[4*i:4*(i+1)].strip('[] '))
+            gg.append(tmp)
+        rows = [[j for j in i if j!=''][::-1] for i in list(zip(*gg))]
+        instruct = inp[1].split('\n')
+
+        for i in instruct:
+            qt,fro,to = list(map(int,i.split(' ')[1::2]))
+
+            x = rows[fro-1]
+            
+            rows[to-1].extend(x[len(x)-qt:])
+            rows[fro-1] = x[:len(x)-qt]
+
+            # while j!=qt:
+            #     rows[to-1].append(rows[fro-1].pop())
+            #     j+=1
+
+
+        
+        
+        for i in rows:
+            v+=i[-1]
+
+        print(f'Part2: {v}')
 
 
 
@@ -34,5 +85,5 @@ class Day5:
             print()
             self.part2()
 
-solver = Day5('test.txt','input.txt',True)
+solver = Day5('test.txt','input.txt',0)
 solver.solve()
